f4d3a4dcf108ab8ea5cc6b19a7053bef93475cff
Jason-6996 <umidjon95@gmail.com>
2023-05-15 04:13:29 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require("express");
 const app = express();
 
 // pastdagi commentdagilar resume publish qilish uchun kerak bo'lgan codelar
-// const fs = require("fs");
 
-// let user;
-// fs.readFile("database/user.json", "utf-8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data)
-//   }
-// });
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // MongoDB chaqirish //call mongodb
 const db = require("./server").db();
@@ -31,17 +32,31 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => { /* post is used to bring some information with itself and write the information to the database  */
-  // console.log(req);
-  // res.json({ test: "success" });
+  console.log("user entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("someting went wrong");
+    } else {
+      res.end("successfully added");
+    }
+  });
   // TODO: code with db here
 });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-})
 
-app.get("/", function (req, res) {  /*get is used to take the information and read them or receive the information from the database*/
-  res.render("reja");
+app.get("/", function (req, res) {
+  console.log('user entered /');
+  db.collection("plans").find().toArray((err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.render("reja", { items: data });
+    }
+  });
 });
+
 
 module.exports = app;
